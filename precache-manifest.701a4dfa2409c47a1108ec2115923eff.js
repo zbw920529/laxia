6d55a2857ccbacaecfc547bf6d3c5a429b3c649f
zbw920529 <41557219+zbw920529@users.noreply.github.com>
2019-01-29 19:11:06 +0800

    Add files via upload

---
+++ precache-manifest.701a4dfa2409c47a1108ec2115923eff.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "601b7b1914ec28d566a2f1c79c648d8f",
+    "url": "/laxia/static/media/znjc.601b7b19.gif"
+  },
+  {
+    "revision": "c34d29bc1b67027dfb2b",
+    "url": "/laxia/static/css/main.679c8959.chunk.css"
+  },
+  {
+    "revision": "4c3b2a3ff3bdc24040af",
+    "url": "/laxia/static/js/1.4c3b2a3f.chunk.js"
+  },
+  {
+    "revision": "1606fd201f3aa289eca7",
+    "url": "/laxia/static/js/runtime~main.1606fd20.js"
+  },
+  {
+    "revision": "d1c82ee0004505530cfdfe8269f9d587",
+    "url": "/laxia/static/media/banner_gd.d1c82ee0.png"
+  },
+  {
+    "revision": "aeb072b409dacf566b0e057e97adc629",
+    "url": "/laxia/static/media/banner_tm.aeb072b4.png"
+  },
+  {
+    "revision": "94c8d3aea032b1c7fd25a4624e4006fa",
+    "url": "/laxia/static/media/banner_gmi.94c8d3ae.jpg"
+  },
+  {
+    "revision": "c34d29bc1b67027dfb2b",
+    "url": "/laxia/static/js/main.c34d29bc.chunk.js"
+  },
+  {
+    "revision": "20e07ba305e820974f9b5c0252ad9cca",
+    "url": "/laxia/static/media/znph.20e07ba3.gif"
+  },
+  {
+    "revision": "edd0ad5eef3dca28ec3e76f67a2dca56",
+    "url": "/laxia/static/media/image_cyhz.edd0ad5e.jpg"
+  },
+  {
+    "revision": "47bb76b8daa6ef5c0eb201e558d6f5b6",
+    "url": "/laxia/static/media/btn_yge2.47bb76b8.jpg"
+  },
+  {
+    "revision": "4aa6a2c39eebb4c434569642fdfa6dc0",
+    "url": "/laxia/static/media/image_db.4aa6a2c3.png"
+  },
+  {
+    "revision": "9d89d3dff4885af87b88bd8403f67075",
+    "url": "/laxia/static/media/bg_3.9d89d3df.jpg"
+  },
+  {
+    "revision": "a34a34821e3d59f2a0aca7ab077b6f70",
+    "url": "/laxia/index.html"
+  }
+];
